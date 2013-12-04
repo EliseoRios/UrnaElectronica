96847f31881461a591a1520f7bfc4fe0b0798573
EliseoRios <eliseohack@gmail.com>
2013-12-03 22:23:10 -0600

    correo en proceso antes de ciudadanos (casi final)

---
+++ Votaciones/FormCorreo.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Votaciones
+{
+    partial class FormCorreo
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDe = new System.Windows.Forms.TextBox();
+            this.txtPara = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAsunto = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDe
+            // 
+            this.txtDe.Location = new System.Drawing.Point(516, 144);
+            this.txtDe.Name = "txtDe";
+            this.txtDe.Size = new System.Drawing.Size(303, 20);
+            this.txtDe.TabIndex = 0;
+            // 
+            // txtPara
+            // 
+            this.txtPara.Location = new System.Drawing.Point(516, 190);
+            this.txtPara.Name = "txtPara";
+            this.txtPara.Size = new System.Drawing.Size(303, 20);
+            this.txtPara.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(455, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "De";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(455, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Para";
+            // 
+            // txtAsunto
+            // 
+            this.txtAsunto.Location = new System.Drawing.Point(516, 239);
+            this.txtAsunto.Name = "txtAsunto";
+            this.txtAsunto.Size = new System.Drawing.Size(383, 20);
+            this.txtAsunto.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(455, 246);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Asunto";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(766, 303);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormCorreo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1354, 725);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAsunto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPara);
+            this.Controls.Add(this.txtDe);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormCorreo";
+            this.Text = "Enviar Resultados";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDe;
+        private System.Windows.Forms.TextBox txtPara;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAsunto;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Votaciones/FormCorreo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Mail;
+
+namespace Votaciones
+{
+    public partial class FormCorreo : Form
+    {
+        public FormCorreo()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MailMessage msg = new MailMessage();
+
+            msg.To.Add(new MailAddress(txtPara.Text));
+            msg.From = new MailAddress(txtDe.Text);
+            msg.Subject = txtAsunto.Text;
+            msg.Body = "El contenido del mensaje";
+
+            SmtpClient clienteSmtp = new SmtpClient("WIN02");
+
+            clienteSmtp.Credentials =
+            new NetworkCredential("usuario", "clave");
+
+            try
+            {
+                clienteSmtp.Send(msg);
+            }
+
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+
+    }
+}
